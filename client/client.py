39ca046b407751378611c5987fc9dc4da76e4eb9
CapybaraV <yskiwolf01@rambler.ru>
2026-03-30 05:23:43 +0300

    update client.py

--- client/client.py
+++ client/client.py
@@ -6,27 +6,86 @@ import service_pb2
 import service_pb2_grpc
 import json
 
-def call_execute_script():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = service_pb2_grpc.ScriptServiceStub(channel)
-    params = {
-            "start_x": 1,
-            "start_y": 1,
-            "end_x": 2,
-            "end_y": 2
-        }
-    request = service_pb2.ScriptRequest(
-        url="http://0.0.0.0:8000/script.py",
-        params = json.dumps(params)
-    )
-    try:
-        response = stub.ExecuteScript(request)
-        print(response)
-        return response
-    except grpc.RpcError as e:
-        print(f"{e.code()} - {e.details()}")
-    except Exception as e:
-        print(f"{e}")
+class Client:
+    def __init__(self, address='localhost:50051'):
+        self.channel = grpc.insecure_channel(address)
+        self.stub = service_pb2_grpc.ScriptServiceStub(self.channel)
+    def input_url(self):
+        url = input("Введите url:\n").strip()
+        return url
+    def input_params(self):
+        params = input("Введите параметры в формате JSON:\n")
+        try:
+            params = json.loads(params)
+            return {str(k): str(v) for k,v in params.items()}
+        except Exception as e:
+            print(e)
+    def run_sync(self):
+        request = service_pb2.ScriptRequest(
+            url = self.input_url(),
+            params = self.input_params()
+        )
+        try:
+            response = self.stub.ExecuteScript(request)
+            print(response)
+            return response
+        except grpc.RpcError as e:
+            print(f"{e.code()} - {e.details()}")
+        except Exception as e:
+            print(f"{e}")
+    def run(self):
+        print("===Python_aaS===\n")
+        while True:
+            print("Выберите действие:\n")
+            print("1 - Sync запуск\n")
+            print("2 - Async запуск\n")
+            print("3 - Узнать статус\n")
+            print("4 - Получить результат\n")
+            print("0 - Выход\n")
+            answer = input().strip()
+            match answer:
+                case '1':
+                    self.run_sync()
+                case '2':
+                    self.run_async()
+                case '3':
+                    self.return_status()
+                case '4':
+                    self.get_res()
+                case '0':
+                    break
+                case _:
+                    print("Неверный ввод\n")
+
+
+
+
+# def call_execute_script():
+#     channel = grpc.insecure_channel('localhost:50051')
+#     stub = service_pb2_grpc.ScriptServiceStub(channel)
+#     params = {
+#             "start_x": 1,
+#             "start_y": 1,
+#             "end_x": 2,
+#             "end_y": 2
+#         }
+#     request = service_pb2.ScriptRequest(
+#         url="http://0.0.0.0:8000/script.py",
+#         params = json.dumps(params)
+#     )
+#     try:
+#         response = stub.ExecuteScript(request)
+#         print(response)
+#         return response
+#     except grpc.RpcError as e:
+#         print(f"{e.code()} - {e.details()}")
+#     except Exception as e:
+#         print(f"{e}")
+
+def start_client():
+    client = Client()
+    client.run()
+
 
 if __name__ == "__main__":
-    call_execute_script()
+    start_client()
